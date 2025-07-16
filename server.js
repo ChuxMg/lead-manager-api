b28250e10d36d4da5da759d57884ecdac8ce4c8d
ChuxMg <chuxmgbojikwe@gmail.com>
2025-07-17 01:34:27 +0700

    Implement api setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const leadsRoute = require("./routes/leads");
+
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+app.use("/leads", leadsRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
